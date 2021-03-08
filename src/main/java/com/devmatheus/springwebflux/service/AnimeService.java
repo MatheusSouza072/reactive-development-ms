5a26b11b0d32f294511b0b93488d7474f0e5702d
MatheusSouza072 <matheus.070202@gmail.com>
2021-03-08 17:20:55 -0300

    Delete reativo

--- src/main/java/com/devmatheus/springwebflux/service/AnimeService.java
+++ src/main/java/com/devmatheus/springwebflux/service/AnimeService.java
@@ -3,10 +3,8 @@ package com.devmatheus.springwebflux.service;
 import com.devmatheus.springwebflux.domain.Anime;
 import com.devmatheus.springwebflux.repository.AnimeRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,15 +13,18 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class AnimeService {
 
-private final AnimeRepository animeRepository;
+    private final AnimeRepository animeRepository;
+
     public Flux<Anime> findAll() {
         return animeRepository.findAll();
     }
+
     public Mono<Anime> findById(int id) {
         return animeRepository.findById(id)
-            .switchIfEmpty(monoResponseStatusNotFoundException());
+                .switchIfEmpty(monoResponseStatusNotFoundException());
 
     }
+
     public <T> Mono<T> monoResponseStatusNotFoundException() {
         return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
     }
@@ -34,8 +35,13 @@ private final AnimeRepository animeRepository;
 
     public Mono<Void> update(Anime anime) {
         return findById(anime.getId())
-                .map(animeFound -> anime.withId(animeFound.getId()))
                 .flatMap(animeRepository::save)
-                .thenEmpty(Mono.empty());
+                .then();
     }
+
+    public Mono<Void> delete(int id) {
+        return findById(id)
+                .flatMap(animeRepository::delete);
+    }
+
 }
