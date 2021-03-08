fac7fe1bb5bb62304dee8818ab2bbbb6c6cd2503
MatheusSouza072 <matheus.070202@gmail.com>
2021-03-08 15:27:50 -0300

    Refatorando parametro stacl-trace da URI

--- src/main/java/com/devmatheus/springwebflux/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devmatheus/springwebflux/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devmatheus.springwebflux.exception;
 
+import io.netty.util.internal.StringUtil;
 import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
@@ -9,6 +10,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.*;
@@ -21,31 +23,32 @@ import java.util.Optional;
 @Order(-2)
 public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
 
-    public GlobalExceptionHandler(ErrorAttributes errorAttributes, ResourceProperties resourceProperties, ApplicationContext applicationContext, ServerCodecConfigurer codecConfigurer) {
+    public GlobalExceptionHandler(ErrorAttributes errorAttributes,
+                                  ResourceProperties resourceProperties,
+                                  ApplicationContext applicationContext,
+                                  ServerCodecConfigurer codecConfigurer) {
         super(errorAttributes, resourceProperties, applicationContext);
-        this.setMessageWriters(codecConfigurer. getWriters());
+        this.setMessageWriters(codecConfigurer.getWriters());
     }
 
-
-    /*
-    @Override
-    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
-        return RouterFunctions.route(RequestPredicates.all(), this::formatErrorResponse);
-    }
-
-    */
-     
     @Override
     protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
         return RouterFunctions.route(RequestPredicates.all(), this::formatErrorResponse);
     }
 
     private Mono<ServerResponse> formatErrorResponse(ServerRequest request) {
-        Map<String, Object> errorAttributesMap = getErrorAttributes (request, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE));
+        String query = request.uri().getQuery();
+        ErrorAttributeOptions errorAttributeOptions = isTraceEnabled(query) ? of(Include.STACK_TRACE) : defaults();
+
+        Map<String, Object> errorAttributesMap = getErrorAttributes(request, errorAttributeOptions);
         int status = (int) Optional.ofNullable(errorAttributesMap.get("status")).orElse(500);
-        return ServerResponse.status(status)
+        return ServerResponse
+                .status(status)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(errorAttributesMap));
+    }
 
+    private boolean isTraceEnabled(String query) {
+        return !StringUtils.isEmpty(query) && query.contains("trace=true");
     }
 }
