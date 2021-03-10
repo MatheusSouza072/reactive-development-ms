252bb03afef8ad7406f8d8c0389f823dc9dab70d
MatheusSouza072 <matheus.070202@gmail.com>
2021-03-09 21:28:02 -0300

    Teste de integração do Webflux com WebTestClient parte 4

--- src/test/java/com/devmatheus/springwebflux/SpringWebfluxApplicationTests.java
+++ src/test/java/com/devmatheus/springwebflux/SpringWebfluxApplicationTests.java
@@ -3,10 +3,10 @@ package com.devmatheus.springwebflux;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringWebfluxApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
